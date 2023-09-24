96e0b706df531a90b24ea24f7926c4da94302023
Aloys <17796338+abitbetterthanyesterday@users.noreply.github.com>
2023-09-24 19:21:08 +1000

    Set up Mantine

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -2,14 +2,19 @@ import { AppProps, ErrorBoundary } from "@blitzjs/next"
 import React, { Suspense } from "react"
 import { withBlitz } from "src/blitz-client"
 import "src/styles/globals.css"
+import "@mantine/core/styles.css"
 import { RootErrorFallback } from "@/core/components/RootErrorFallback"
+import { Button, MantineProvider, Popover } from "@mantine/core"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ErrorBoundary FallbackComponent={RootErrorFallback}>
-      <Suspense fallback="Loading...">
-        <Component {...pageProps} />
-      </Suspense>
+      <MantineProvider defaultColorScheme={"dark"}>
+        <Suspense fallback="Loading...">
+          <Component {...pageProps} />
+          <Button>Hello</Button>
+        </Suspense>
+      </MantineProvider>
     </ErrorBoundary>
   )
 }
--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -1,10 +1,14 @@
 import Document, { Html, Main, NextScript, Head } from "next/document"
 
+import { Button, ColorSchemeScript } from "@mantine/core"
+
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <ColorSchemeScript defaultColorScheme={"dark"} />
+        </Head>
         <body>
           <Main />
           <NextScript />
