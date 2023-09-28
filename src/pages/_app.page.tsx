30054a2b9ea292e9de91e59b17a176a33ac790dd
Aloys <17796338+abitbetterthanyesterday@users.noreply.github.com>
2023-09-28 18:07:18 +1000

    Add Mantine's notifications

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -3,13 +3,16 @@ import React, { Suspense } from "react"
 import { withBlitz } from "src/blitz-client"
 import "src/styles/globals.css"
 import "@mantine/core/styles.css"
+import "@mantine/notifications/styles.css"
 import { RootErrorFallback } from "@/core/components/RootErrorFallback"
 import { MantineProvider } from "@mantine/core"
+import { Notifications } from "@mantine/notifications"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ErrorBoundary FallbackComponent={RootErrorFallback}>
       <MantineProvider defaultColorScheme={"dark"}>
+        <Notifications position={"top-center"} />
         <Suspense fallback="Loading...">
           <Component {...pageProps} />
         </Suspense>
--- src/pages/ads/new/glider.page.tsx
+++ src/pages/ads/new/glider.page.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Text, TextInput } from "@mantine/core"
+import { Button, Text, TextInput } from "@mantine/core"
 import { BlitzPage } from "@blitzjs/next"
 import Layout from "@/core/layouts/Layout"
 import { Form, useForm } from "@mantine/form"
@@ -7,6 +7,8 @@ import { useMutation } from "@blitzjs/rpc"
 import createGlider from "@/gliders/mutations/createGlider"
 import { CreateGliderSchema } from "@/gliders/mutations/createGlider"
 import { z } from "zod"
+import { notifications } from "@mantine/notifications"
+import { IconCheck } from "@tabler/icons-react"
 
 type Values = z.infer<typeof CreateGliderSchema>
 const NewGliderPage: BlitzPage = () => {
@@ -22,7 +24,13 @@ const NewGliderPage: BlitzPage = () => {
   const [$createGlider] = useMutation(createGlider)
 
   async function handleSubmit(values) {
-    await $createGlider(values)
+    // await $createGlider(values)
+    notifications.show({
+      title: "Glider created",
+      message: "Your glider has been created",
+      color: "green",
+      icon: <IconCheck />,
+    })
   }
 
   //TODO Add the rest of the fields
@@ -30,6 +38,7 @@ const NewGliderPage: BlitzPage = () => {
     <Layout title={"New glider"}>
       <form onSubmit={form.onSubmit(handleSubmit)}>
         <TextInput {...form.getInputProps("name")} placeholder={"name"} />
+        <Button type={"submit"}>Submit</Button>
       </form>
     </Layout>
   )
