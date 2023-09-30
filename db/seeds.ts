e0349c9953ba64b74927056d1be4226b1689432c
Aloys <17796338+abitbetterthanyesterday@users.noreply.github.com>
2023-09-30 15:30:28 +1000

    Add fixtures/seed for glider model and brand.

--- db/seeds.ts
+++ db/seeds.ts
@@ -6,10 +6,26 @@
  * Probably you want to use a library like https://chancejs.com
  * to easily generate realistic data.
  */
+import { gliders } from "./fixtures/gliderBrand"
+import db from "./index"
+
 const seed = async () => {
-  // for (let i = 0; i < 5; i++) {
-  //   await db.project.create({ data: { name: "Project " + i } })
-  // }
+  await seedGliderBrandsAndModels()
 }
 
 export default seed
+
+/** Seed glider brands and models */
+async function seedGliderBrandsAndModels() {
+  for (const [brand, models] of Object.entries(gliders)) {
+    const { id } = await db.gliderBrand.create({ data: { name: brand } })
+    for (const model of models) {
+      await db.gliderModel.create({
+        data: {
+          name: model,
+          brand: { connect: { id } },
+        },
+      })
+    }
+  }
+}
