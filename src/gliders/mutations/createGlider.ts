3788f0c0b9dc96798db113fdeec263cb3f76153c
Aloys <17796338+abitbetterthanyesterday@users.noreply.github.com>
2023-09-30 14:46:37 +1000

    Scaffolding the create a glider form

--- src/gliders/mutations/createGlider.ts
+++ src/gliders/mutations/createGlider.ts
@@ -2,13 +2,15 @@ import { resolver } from "@blitzjs/rpc"
 import { z } from "zod"
 
 export const CreateGliderSchema = z.object({
-  year: z.date(),
+  year: z.number().min(1979).max(2050),
   size: z.string().optional(),
   minWeight: z.number().optional(),
   maxWeight: z.number().optional(),
   hours: z.number().optional(),
   description: z.string().optional(),
   price: z.number(),
+  model: z.string(),
+  brand: z.string(),
 })
 export default resolver.pipe(resolver.zod(CreateGliderSchema), resolver.authorize(), async () => {
   return
--- src/pages/ads/new/glider.page.tsx
+++ src/pages/ads/new/glider.page.tsx
@@ -1,5 +1,22 @@
 import React from "react"
-import { Button, Text, TextInput } from "@mantine/core"
+import {
+  Autocomplete,
+  Box,
+  Button,
+  Center,
+  Checkbox,
+  Container,
+  Flex,
+  Group,
+  NumberInput,
+  Paper,
+  RangeSlider,
+  SegmentedControl,
+  Stack,
+  Text,
+  Textarea,
+  TextInput,
+} from "@mantine/core"
 import { BlitzPage } from "@blitzjs/next"
 import Layout from "@/core/layouts/Layout"
 import { Form, useForm } from "@mantine/form"
@@ -9,15 +26,22 @@ import { CreateGliderSchema } from "@/gliders/mutations/createGlider"
 import { z } from "zod"
 import { notifications } from "@mantine/notifications"
 import { IconCheck } from "@tabler/icons-react"
+import { CurrencyInput } from "@/core/components/CurrencyInput"
+import { useWatch } from "react-hook-form"
 
 type Values = z.infer<typeof CreateGliderSchema>
 const NewGliderPage: BlitzPage = () => {
   const form = useForm<Values>({
     initialValues: {
       // TODO: Add the remaing of the fields and connect to the form
-      year: new Date(),
+      year: new Date().getFullYear(),
       description: "",
       price: 0,
+      minWeight: 60,
+      maxWeight: 90,
+      brand: "",
+      model: "",
+      hours: 50,
     },
   })
 
@@ -33,13 +57,132 @@ const NewGliderPage: BlitzPage = () => {
     })
   }
 
-  //TODO Add the rest of the fields
+  const marks = [
+    { value: 40, label: "40" },
+    { value: 60, label: "60" },
+    { value: 80, label: "80" },
+    { value: 100, label: "100" },
+    { value: 120, label: "120" },
+    { value: 140, label: "140" },
+  ]
+
+  //TODO Connect to the backend
+  //TODO make responsive on mobile
   return (
     <Layout title={"New glider"}>
-      <form onSubmit={form.onSubmit(handleSubmit)}>
-        <TextInput {...form.getInputProps("name")} placeholder={"name"} />
-        <Button type={"submit"}>Submit</Button>
-      </form>
+      <Container size={"sm"} h={"100%"}>
+        <form onSubmit={form.onSubmit(handleSubmit)}>
+          <Stack>
+            <Text size={"xl"}>New Glider Ad</Text>
+
+            <Paper w={"100%"} bg={"gray.9"} h={350} radius={"md"} shadow={"lg"}>
+              <Stack align={"center"} justify={"center"} h={"100%"}>
+                <Text c={"dimmed"}>Coming soon</Text>
+                <Text c={"dimmed"}>Upload images</Text>
+              </Stack>
+            </Paper>
+
+            <Autocomplete
+              description={
+                "If you can't find your brand, select 'other' and add it in the description"
+              }
+              label={"Brand"}
+              placeholder={"Advance, Skywalk..."}
+              data={["Advance", "Skywalk"]}
+              {...form.getInputProps("brand")}
+              onChange={(e) => {
+                const hasChanged = form.values["brand"] !== e
+                form.getInputProps("model").onChange(e)
+                if (!hasChanged) {
+                  form.setValues({ ...form.values, model: "" })
+                }
+              }}
+            />
+
+            <Autocomplete
+              description={"The model of your glider"}
+              label={"Model"}
+              placeholder={"Advance, Skywalk..."}
+              data={["Advance", "Skywalk"]}
+              disabled={form.values["brand"]?.length === 0}
+              {...form.getInputProps("model")}
+            />
+
+            <Stack gap={2}>
+              <Text size={"sm"}>Type</Text>
+              <SegmentedControl
+                data={["Cross country", "Acro", "Tandem", "Mini", "Speed", "Other"]}
+              />
+            </Stack>
+
+            <Stack gap={2}>
+              <Text size={"sm"}>Certification</Text>
+              <SegmentedControl data={["A", "B", "B+", "C", "D", "Competition", "Open"]} />
+            </Stack>
+
+            <Stack gap={2}>
+              <Text size={"sm"}>Weight range</Text>
+              <RangeSlider
+                step={5}
+                min={30}
+                label={null}
+                marks={marks}
+                max={150}
+                value={[form.values["minWeight"] ?? 60, form.values["maxWeight"] ?? 90]}
+                onChange={(value) => {
+                  form.setFieldValue("minWeight", value[0])
+                  form.setFieldValue("maxWeight", value[1])
+                }}
+                thumbSize={"40"}
+                thumbChildren={[
+                  <Text key={"min"} size={"sm"}>
+                    {form.values["minWeight"]}
+                  </Text>,
+                  <Text key={"max"} size={"sm"}>
+                    {form.values["maxWeight"]}
+                  </Text>,
+                ]}
+              />
+            </Stack>
+
+            <NumberInput
+              description={"You can find this information on the tag inside your glider"}
+              min={1979}
+              max={new Date().getFullYear()}
+              label={"Year"}
+              {...form.getInputProps("year")}
+            />
+
+            <NumberInput
+              min={0}
+              max={1000}
+              label={"Hours"}
+              description={"If you are not sure, give an estimate"}
+              {...form.getInputProps("hours")}
+            />
+
+            <Stack gap={2}>
+              <Text>Extras</Text>
+              <Stack gap={8}>
+                <Checkbox label="Comes with a certificate of inspection" />
+                <Checkbox label={"Comes with the glider backpack"} />
+                <Checkbox label={"Comes with a concertina / sausage bag"} />
+              </Stack>
+            </Stack>
+
+            <Textarea
+              label={"Description"}
+              description={"Anything you want to add?"}
+              placeholder={"In great condition, bought last year."}
+              {...form.getInputProps("description")}
+            />
+
+            <CurrencyInput label={"Asking price"} {...form.getInputProps("price")} />
+
+            <Button type={"submit"}>Submit</Button>
+          </Stack>
+        </form>
+      </Container>
     </Layout>
   )
 }
