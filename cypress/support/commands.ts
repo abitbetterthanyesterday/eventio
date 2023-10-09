24c5774b5f077ee448af1c0519880cf307468b63
Aloys <17796338+abitbetterthanyesterday@users.noreply.github.com>
2023-10-09 13:32:14 +1000

    Add Cypress idle network

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,5 +1,6 @@
 import "@testing-library/cypress/add-commands"
 import "cypress-real-events/support"
+import "cypress-network-idle"
 
 /// <reference types="cypress" />
 // ***********************************************
