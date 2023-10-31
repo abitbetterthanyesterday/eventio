2e097c6f2b1d63dde97853149b763f4f3e50ceda
Aloys <17796338+abitbetterthanyesterday@users.noreply.github.com>
2023-10-31 15:50:10 +1000

    refactor(AuthenticationForm): Reorganize component structure and improve readability

--- src/auth/components/AuthenticationForm/AuthenticationForm.tsx
+++ src/auth/components/AuthenticationForm/AuthenticationForm.tsx
@@ -24,8 +24,27 @@ import { Devtoolbox } from "@/core/components/DevToolbox"
 import { notifications } from "@mantine/notifications"
 
 export const AuthenticationForm = (props: PaperProps) => {
-  const { error, type, toggleType, form, onSubmit } = useAuthenticationForm()
-  const onChangeTerms = (event) => form.setFieldValue("terms", event.currentTarget.checked)
+  return (
+    <Container pt={"10vh"} size={"xs"}>
+      <Devtoolbox />
+
+      <Paper radius="md" p="xl" withBorder {...props}>
+        <Text size="lg" fw={500}>
+          Welcome to Paramarket
+        </Text>
+
+        <GoogleLoginButton />
+
+        <Form />
+
+        <Divider label="Or continue with email" labelPosition="center" my="lg" />
+      </Paper>
+    </Container>
+  )
+}
+
+const Form = () => {
+  const { error, type, toggleType, form, onSubmit, onChangeTerms } = useAuthenticationForm()
 
   const onClickAlert = () => {
     notifications.show({
@@ -36,77 +55,69 @@ export const AuthenticationForm = (props: PaperProps) => {
   }
 
   return (
-    <Container pt={"10vh"} size={"xs"}>
-      <Devtoolbox />
+    <form
+      onSubmit={form.onSubmit((data: Values) => void onSubmit(data))}
+      name={"login"}
+      title={type}
+    >
+      <Stack align={"stretch"}>
+        {error && <AuthenticationErrorAlert error={error} onClick={onClickAlert} />}
 
-      <Paper radius="md" p="xl" withBorder {...props}>
-        <Text size="lg" fw={500}>
-          Welcome to Paramarket, {type} with
-        </Text>
+        {type === "register" && (
+          <TextInput
+            label="Name"
+            placeholder="Your name"
+            {...form.getInputProps("name")}
+            radius="md"
+          />
+        )}
 
-        <Tooltip label={"Coming soon"}>
-          <Stack py={12}>
-            <GoogleButton radius="xl" disabled opacity={0.5} />
-          </Stack>
-        </Tooltip>
+        <TextInput
+          required
+          label="Email"
+          placeholder="hello@mantine.dev"
+          {...form.getInputProps("email")}
+          radius="md"
+        />
 
-        <Divider label="Or continue with email" labelPosition="center" my="lg" />
+        <PasswordInput
+          required
+          label="Password"
+          placeholder="Your password"
+          radius="md"
+          role={"password"}
+          {...form.getInputProps("password")}
+        />
 
-        <form
-          onSubmit={form.onSubmit((data: Values) => void onSubmit(data))}
-          name={"login"}
-          title={type}
-        >
-          <Stack align={"stretch"}>
-            {error && <AuthenticationErrorAlert error={error} onClick={onClickAlert} />}
+        {type === "register" && (
+          <Checkbox
+            label="I accept terms and conditions"
+            checked={form.values.terms}
+            onChange={onChangeTerms}
+          />
+        )}
+      </Stack>
 
-            {type === "register" && (
-              <TextInput
-                label="Name"
-                placeholder="Your name"
-                {...form.getInputProps("name")}
-                radius="md"
-              />
-            )}
+      <Group justify="space-between" mt="xl">
+        <Anchor component="button" type="button" c="dimmed" onClick={toggleType} size="xs">
+          {type === "register"
+            ? "Already have an account? Login"
+            : "Don't have an account? Register"}
+        </Anchor>
+        <Button type="submit" radius="xl">
+          {upperFirst(type)}
+        </Button>
+      </Group>
+    </form>
+  )
+}
 
-            <TextInput
-              required
-              label="Email"
-              placeholder="hello@mantine.dev"
-              {...form.getInputProps("email")}
-              radius="md"
-            />
-
-            <PasswordInput
-              required
-              label="Password"
-              placeholder="Your password"
-              radius="md"
-              role={"password"}
-              {...form.getInputProps("password")}
-            />
-
-            {type === "register" && (
-              <Checkbox
-                label="I accept terms and conditions"
-                checked={form.values.terms}
-                onChange={onChangeTerms}
-              />
-            )}
-          </Stack>
-
-          <Group justify="space-between" mt="xl">
-            <Anchor component="button" type="button" c="dimmed" onClick={toggleType} size="xs">
-              {type === "register"
-                ? "Already have an account? Login"
-                : "Don't have an account? Register"}
-            </Anchor>
-            <Button type="submit" radius="xl">
-              {upperFirst(type)}
-            </Button>
-          </Group>
-        </form>
-      </Paper>
-    </Container>
+const GoogleLoginButton = (): JSX.Element => {
+  return (
+    <Tooltip label={"Coming soon"}>
+      <Stack py={12}>
+        <GoogleButton radius="xl" disabled opacity={0.5} />
+      </Stack>
+    </Tooltip>
   )
 }
