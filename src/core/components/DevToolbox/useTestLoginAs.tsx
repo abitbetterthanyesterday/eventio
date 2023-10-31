8bcaa1cb7b9926f5287ebdc235520824e841556a
Aloys <17796338+abitbetterthanyesterday@users.noreply.github.com>
2023-10-31 15:55:09 +1000

    feat(DevToolbox): Add environment variable checks and async handling to loginAs functions

--- src/core/components/DevToolbox/useTestLoginAs.tsx
+++ src/core/components/DevToolbox/useTestLoginAs.tsx
@@ -1,17 +1,24 @@
 import { useMutation } from "@blitzjs/rpc"
 import login from "@/auth/mutations/login"
+import { process } from "std-env"
 
 export function useTestLoginAs() {
   const [$login] = useMutation(login)
 
   async function loginAs(role: "admin" | "user") {
-    let email, password
+    let email: string, password: string
     switch (role) {
       case "admin":
+        if (!process.env.NEXT_PUBLIC_ADMIN_EMAIL || !process.env.NEXT_PUBLIC_ADMIN_PASSWORD) {
+          throw new Error(`Missing ${role} credentials in env. file`)
+        }
         email = process.env.NEXT_PUBLIC_ADMIN_EMAIL
         password = process.env.NEXT_PUBLIC_ADMIN_PASSWORD
         break
       case "user":
+        if (!process.env.NEXT_PUBLIC_USER_EMAIL || !process.env.NEXT_PUBLIC_USER_PASSWORD) {
+          throw new Error(`Missing ${role} credentials in env. file`)
+        }
         email = process.env.NEXT_PUBLIC_USER_EMAIL
         password = process.env.NEXT_PUBLIC_USER_PASSWORD
         break
@@ -26,12 +33,12 @@ export function useTestLoginAs() {
     })
   }
 
-  function loginAsAdmin() {
-    loginAs("admin")
+  async function loginAsAdmin() {
+    await loginAs("admin")
   }
 
-  function loginAsUser() {
-    loginAs("user")
+  async function loginAsUser() {
+    await loginAs("user")
   }
 
   return { loginAsAdmin, loginAsUser }
